ae6e2f84b2707a22dbdc2c934aab6bc19474365c
Dzhivanyan <david@ubiy.ru>
2024-10-08 00:48:48 +0300

    [master] - fix

--- src/utils/captcha.ts
+++ src/utils/captcha.ts
@@ -25,25 +25,3 @@ export function playAnimationWithPromise (lottieRef: LottieRefCurrentProps, pare
     }
   });
 }
-
-export function setDirection(xElem: number, xClick: number): number {
-  if (xElem > xClick) {
-    return 0
-  } else {
-    return 1;
-  }
-}
-
-
-export function setPosition(elem: HTMLElement, x: number, container: RefObject<HTMLDivElement>) {
-  if (x > container.current!.offsetWidth / 2) {
-    const rightInPixels = container.current!.offsetWidth - x - elem.offsetWidth;
-    const rightInPercent = (rightInPixels / container.current!.offsetWidth) * 100;
-    elem.style.right = `${rightInPercent}%`;
-    elem.style.left = 'initial';
-  } else {
-    const leftInPercent = (x / container.current!.offsetWidth) * 100; // вычисляем в процентах
-    elem.style.right = 'initial';
-    elem.style.left = `${leftInPercent}%`;
-  }
-}
